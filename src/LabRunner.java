5adc8d4cee189a3ff3322344c1ed748161639acd
1E10 Laptop <sample@bths.edu>
2022-10-11 11:00:36 -0400

    ili

---
+++ src/LabRunner.java
@@ -0,0 +1,57 @@
+public class LabRunner {
+    public static void main(String[]args){
+//        String str1 = "Hello!";  System.out.println(str1.length());
+//
+//        String str2 = " H e l l o ! ";
+//        int len = str2.length();
+//        System.out.println(len);
+//
+//        String str3 = "Is tax 8.5%?";
+//        System.out.println(str3.length());
+//
+//        String str4 = "43";
+//        int len2 = str4.length();
+//        System.out.println(len2);
+//
+//        String str5 = "";
+//        System.out.println(str5.length());
+//
+//        String str6 = "Cat says \"MEOW\"!";
+//        System.out.println(str6.length());
+//
+//        String str7 = "\\\"\"\n\\\n";
+//        System.out.println(str7.length());
+        /**String str1 = "Hello how are you today?";
+        System.out.println(str1.length());
+        System.out.println(str1.indexOf("H"));
+        System.out.println(str1.indexOf("h"));
+        System.out.println(str1.indexOf("l"));
+        System.out.println(str1.indexOf("o"));
+        System.out.println(str1.indexOf("?"));
+        System.out.println(str1.indexOf("ll"));
+        System.out.println(str1.indexOf("ello"));
+        System.out.println(str1.indexOf("lo ho"));
+        System.out.println(str1.indexOf("a"));
+        System.out.println(str1.indexOf("W"));
+        System.out.println(str1.indexOf(" "));
+        System.out.println(str1.indexOf("how are"));
+        System.out.println(str1.indexOf("howare"));
+        System.out.println(str1.indexOf("Are"));
+        System.out.println(str1.indexOf("Hello how are you today?"));
+        System.out.println(str1.indexOf(""));
+
+        int x = str1.indexOf("e") + str1.indexOf("E");
+        System.out.println(x);
+
+        String str2 = "how are you?";
+        System.out.println(str1.indexOf(str2));**/
+
+        String myString = "AP CSA is awesome!";
+        String s = myString.substring(4,5);
+        System.out.print(s);
+        String today = "FRIDAY!";
+        String x = today.substring(3, 7).substring(0, 3).substring(1, 3);
+        System.out.println(x);
+
+    }
+}
